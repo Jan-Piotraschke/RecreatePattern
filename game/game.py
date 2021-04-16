82deac42c64f16ff7ff68c1bc66e37cb49afadc7
Jan Piotraschke <janpiotraschke@teehouse.localdomain>
2021-04-16 11:27:12 +0200

    implemented the screen manager and the .kv design file for the screen 'game'

--- game/game.py
+++ game/game.py
@@ -1,15 +1,32 @@
+import os
+
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
-
+from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 
 # our .kv design file 
-Builder.load_file('design/game.kv')
+# Builder.load_file('design/game.kv')
 
+down = os.path.join("img", "down.png")
+normal = os.path.join("img", "up.png")
 
-class GameScreen(GridLayout):
-
+class GameScreen(Screen):
     def __init__(self, **kwargs):
         super(GameScreen, self).__init__(**kwargs)
+
+        footer = BoxLayout(orientation="horizontal", size_hint_max_y=40)
+        # footer.add_widget(Button(text="Restart", on_press=self.restart))
+        footer.add_widget(Button(text="Back to menu", on_press=self.back))
+
+        layout = BoxLayout(orientation="vertical")
+        layout.add_widget(footer)
+
+        self.add_widget(layout)
+
+    def back(self, btn):
+        self.parent.transition.direction = "right"
+        self.parent.current = "menu"
--- game/menu.py
+++ game/menu.py
@@ -1,7 +1,3 @@
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 
@@ -14,5 +10,5 @@ class MenuScreen(Screen):
 
     # def new_game(self, btn):
     def new_game(self):
-
+        self.parent.transition.direction = "left"
         self.parent.current = "game"
